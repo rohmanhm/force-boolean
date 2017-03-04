7fed24dbcdcbd59919f74cd602481f6eb998242a
Muhammad Habib Rohman <mhrohman@live.com>
2017-03-04 18:03:41 +0700

    fix writing

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ module.exports = (value) => {
       return value
       break
     case 'number': // (0 | 1)
-      return ( value === 1 )
+      return (value === 1)
       break
     case 'object': // (null)
       return !(value === null);
       break;
     case 'string': // ('true' | 'false' | '0' | '1')
-      return ( value === 'true' || value === '1' )
+      return (value === 'true' || value === '1')
       break
     case 'undefined': // (undefined)
-      return false;
-      break;
+      return false
+      break
   }
 }
